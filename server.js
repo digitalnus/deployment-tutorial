1b05c3a364ea89abe55b66e036a4b20812885975
Appscorico <87219263+digitalnus@users.noreply.github.com>
2021-12-19 20:17:27 +0800

    server.js update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello, this is Alfred');
+  res.send('Hello, This is CI/CD pipeline setup to push codes to Beanstalk whenever main branch gets updated');
 });
 
 app.listen(PORT, HOST);
