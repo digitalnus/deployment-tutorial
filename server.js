785c5827f0b163b14b86163260c09bc0bab872b4
Appscorico <87219263+digitalnus@users.noreply.github.com>
2021-12-19 19:03:06 +0800

    Changed helloworld message, testing deployment to beanstalk

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World from Nodejs ver2');
+  res.send('Hello, this is Alfred');
 });
 
 app.listen(PORT, HOST);
